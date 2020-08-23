6595cc6ce24fc61c9714efbc85006a4b7b72dcd7
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-08-23 15:17:37 -0400

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Header from './container/header/Header';
 import MainContent from './container/maincontent/MainContent';
 import Footer from './components/footer/Footer';
 import "./App.scss";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return ( 
     <Fragment>
-		<BrowserRouter>
+		<HashRouter>
     		<Header /> 	
         	<MainContent />    	
       		<Footer />
-		</BrowserRouter>
+		</HashRouter>
     </Fragment>  
   );
 };
