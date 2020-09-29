48a1df9d54b62ece97253c7414ae936fb98bcdf9
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-09-29 11:12:58 -0400

    Updates

---
+++ precache-manifest.452715e29e6c2ae885137192903d70b9.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544f91dd41e3b5d784c2ab78c966ebfa",
+    "url": "/georgia-gifts-react/index.html"
+  },
+  {
+    "revision": "6be7d71714cb7b01540e",
+    "url": "/georgia-gifts-react/static/css/2.1f057938.chunk.css"
+  },
+  {
+    "revision": "5df703719658804c74d1",
+    "url": "/georgia-gifts-react/static/css/main.00668737.chunk.css"
+  },
+  {
+    "revision": "6be7d71714cb7b01540e",
+    "url": "/georgia-gifts-react/static/js/2.7c433c60.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/georgia-gifts-react/static/js/2.7c433c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df703719658804c74d1",
+    "url": "/georgia-gifts-react/static/js/main.e878337e.chunk.js"
+  },
+  {
+    "revision": "e84f2f7e20057de2fcfe",
+    "url": "/georgia-gifts-react/static/js/runtime-main.5d34e7fd.js"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/georgia-gifts-react/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  },
+  {
+    "revision": "56b73a40bb7fb28fdee89df86f8b1188",
+    "url": "/georgia-gifts-react/static/media/rock-salt-latin-400.56b73a40.woff2"
+  },
+  {
+    "revision": "a3e02e5235e70f55685f61891bf1b42a",
+    "url": "/georgia-gifts-react/static/media/rock-salt-latin-400.a3e02e52.woff"
+  }
+]);
